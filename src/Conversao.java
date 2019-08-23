33c31c82c376b0959c8724ab2936959eead5e8a4
Leticia <leticiassantos15@outlook.com>
2019-08-23 10:50:28 -0300

    Primeiro commit

---
+++ src/Conversao.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class Conversao {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner (System.in);
+        
+        String palavra = "";
+        System.out.println( somenteDigitos(palavra));
+     }   
+   
+static int somenteDigitos(String palavra) {
+    String digitos = palavra;
+    palavra= "hdjhdjhddd24322";
+    char[] letras  = palavra.toCharArray();
+    for (char letra : letras) {
+        if(Character.isDigit(letra)) {
+            digitos += letra;
+        }
+    }
+    return Integer.parseInt(digitos);
+} 
+}
